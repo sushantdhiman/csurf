64c2b18b2b22a6dd78d2ac5b49faa6aa34d2003f
Sushant <sushantdhiman@outlook.com>
2021-07-01 11:22:23 +0530

    feat: typings

---
+++ types.d.ts
@@ -0,0 +1,37 @@
+declare module "csurf" {
+  import express from "express-serve-static-core";
+
+  global {
+    namespace Express {
+      interface Request {
+        csrfToken(): string;
+      }
+    }
+  }
+
+  function csurf(options?: {
+    value?: (req: express.Request) => string;
+    /**
+     * @default false
+     */
+    cookie?: csurf.CookieOptions | boolean;
+    ignoreMethods?: string[];
+    sessionKey?: string;
+  }): csurf.RequestHandler;
+
+  namespace csurf {
+    interface CookieOptions extends express.CookieOptions {
+      /**
+       * @default '_csrf'
+       */
+      key?: string;
+    }
+
+    interface RequestHandler extends express.RequestHandler {
+      verify: () => express.RequestHandler;
+      generate: () => express.RequestHandler;
+    }
+  }
+
+  export = csurf;
+}
